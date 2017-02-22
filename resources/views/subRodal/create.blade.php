38828c931a83be66db23d4a0922bcd402be63c55
Tony Garcia <lgarcia-1254@outlook.com>
2017-02-22 08:54:37 -0600

    autocomplete

--- resources/views/subRodal/create.blade.php
+++ resources/views/subRodal/create.blade.php
@@ -1,5 +1,19 @@
 @section('title')Sub Rodales - Crear @endsection
-@extends('layouts.app') @section('content')
+@extends('layouts.app')
+@section('css')
+
+
+@endsection
+@section('scripts')
+<script type="text/javascript">
+
+
+
+</script>
+<script src="/bower_components/EasyAutocomplete/dist/jquery.easy-autocomplete.min.js"></script>
+
+@endsection
+@section('content')
 <div class="container">
     <div class=" col-md-10 col-md-offset-1 text-capitalize">
         <div class="row">
@@ -23,20 +37,10 @@
 
                     {!! Form::open(['action'=>['SubRodalController@store'],'method' => 'post']) !!}
 <p></p>
-<div class="row">
-    <div class="col s12">
-      <div class="row">
-        <div class="input-field col s12">
-          <i class="material-icons prefix">textsms</i>
-          <input type="text" id="autocomplete-input" class="autocomplete">
-          <label for="autocomplete-input">Autocomplete</label>
-        </div>
-      </div>
-    </div>
-  </div>
+
 <div class="row ">
                 <div class="col-sm-2 ">{!! Field::text('objectid',['autofocus','placeholder'=>"0"]) !!}</div>
-                <div class="col-sm-3 ">{!! Field::text('country',[]) !!}</div>
+                <div class="col-sm-3 ">{!! Field::text('country',['placeholder'=>"GT", 'max'=>'2','min'=>'2',"pattern"=>"[A-Za-z]{2}","title"=>'*Debe ser "codigo de Paises [ISO 3166-1 alfa-2]" Ej.: "GT" para Guatemala']) !!}</div>
                 <div class="col-sm-3">{!! Field::text('fund',['placeholder'=>"Aaaa"]) !!}</div>
                 <div class="col-sm-1">{!! Field::text('property',['placeholder'=>"0"]) !!}</div>
                 <div class="col-sm-1">{!! Field::text('rodal',['placeholder'=>"0"]) !!}</div>
@@ -60,9 +64,19 @@
                 <div class="col-sm-1">{!! Field::date('pruning_date',['placeholder'=>"dd-mm-YYYY"]) !!}</div>
                 <div class="col-sm-1">{!! Field::text('pruning_density',['placeholder'=>"0"]) !!}</div>
 
-
             </div>
-
+<input id="example-mail"/>
+ <div class="row">
+    <div class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <i class="material-icons prefix">textsms</i>
+          <input type="text" id="autocomplete-input" class="autocomplete">
+          <label for="autocomplete-input">Autocomplete</label>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
                         {!! Form::button(trans('validation.attributes.save'), ['class' => 'btn teal white-text waves-effect', 'type' =>'sumbit' ]) !!}
@@ -77,38 +91,48 @@
 @section('scripts')
 
 <script type="text/javascript">
+              $( document ).ready(function() {
 
+
+
+                });
 </script>
 @endsection
 @section('endScripts')
 <script type="text/javascript">
+var options = {
+    url: "\\json/countries.json",
+    getValue: "code",
+    template: {
+        type: "description",
+        fields: {
+            description: "name"
+        }
+    },
+    list: {
+        match: {
+            enabled: true
+        }
+    },
+};
 
+//$("#country").easyAutocomplete(options);
               $( document ).ready(function() {
-                    $('#country').autocomplete({
+
+     $('#country').autocomplete({
     data: {
       @foreach ($countries as $element)
-      "{{ $element->name }}": {{ url('flags/'.$element->iso2.'.jpg') }} ,
 
+      "{{ $element->iso2 }} ": '{{  url("content/flags/".$element->iso2.".jpg") }}',
       @endforeach
-
     },
     limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
   });
 
-
-
                     $("#label_plantation_date").attr('class','active');
+                    $("#label_country").attr('class','active');
                     $('select').material_select();
                 });
-    $('#country').click(function() {
 
-    if($('#field_country').is(":visible")){
-        $('#field_country').hide();
-    }else{
-        $('#field_country').show();
-
-    }
-
-});
 </script>
 @endsection
